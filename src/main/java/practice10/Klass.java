2c6d5986d8a5418c164a6748e4857ef92b20da05
lichuwen <545759585@qq.com>
2020-07-23 01:48:02 +0800

    [karen] update practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,45 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    List<Student> students = new ArrayList<>();
+    Klass(int number){
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+    public void assignLeader(Student student){
+        if(students.contains(student)){
+            leader = student;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+    public List appendMember(Student student){
+        students.add(student);
+        if(students != null)
+            return students;
+        return null;
+    }
+
+    public boolean isIn(Student student){
+        if(student.getKlass().equals(this))
+            return true;
+        return false;
+    }
+
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,60 @@
 package practice10;
 
-public class Teacher {
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    Klass klass;
+    String intro;
+    String basicIntro = super.introduce();
+
+    private LinkedList<Klass> classes = new LinkedList<>();
+
+    Teacher(int id, String name, int age, LinkedList<Klass> classes){
+        super(id, name,age);
+        this.classes = classes;
+    }
+    Teacher(int id, String name, int age){
+        super(id, name,age);
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    public String introduce(){
+        if(klass == null)
+            intro = basicIntro + " I am a Teacher. I teach No Class.";
+        else{
+            int[] classTeach = new int[6];
+            int i=0;
+            for (Klass klass : classes) {
+                classTeach[i++] = klass.getNumber();
+            }
+
+            String str = Arrays.toString(classTeach);
+            intro = basicIntro + " I am a Teacher. I teach " + klass.getDisplayName() + String.join(", ", str) + ".";;
+        }
+
+        return intro;
+    }
+    public String introduceWith(Student student){
+        String stuName = student.getName();
+        if(this.klass == student.klass)
+            intro = basicIntro + " I am a Teacher. I teach " + stuName + ".";
+        else
+            intro = basicIntro + " I am a Teacher. I don't teach " + stuName + ".";
+        return intro;
+    }
+    public boolean isTeaching(Student student) {
+        for (Klass klass : classes) {
+            if (klass.isIn(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
