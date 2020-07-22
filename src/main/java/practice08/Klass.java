86e205600ae671444f172665ae9317971ed04f0c
lichuwen <545759585@qq.com>
2020-07-22 18:16:39 +0800

    [karen] update practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,24 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    Klass(int number){
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+    public Student assignLeader(Student student){
+        leader = student;
+        return leader;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,34 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    Klass klass;
+    String intro;
+    String basicInto = super.introduce();
+
+    Teacher(int id, String name, int age){
+        super(id, name,age);
+    }
+    Teacher(int id, String name, int age, Klass klass){
+        super(id, name,age);
+        this.klass = klass;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(klass != null)
+            intro = basicInto + " I am a Teacher. I teach " + klass.getDisplayName() + ".";
+        else
+            intro = basicInto + " I am a Teacher. I teach No Class.";
+        return intro;
+    }
+    public String introduceWith(Student student){
+        String stuName = student.getName();
+        if(this.klass == student.klass)
+            intro = basicInto + " I am a Teacher. I teach " + stuName + ".";
+        else
+            intro = basicInto + " I am a Teacher. I don't teach " + stuName + ".";
+        return intro;
+    }
 }
