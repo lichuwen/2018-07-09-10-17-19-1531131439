f3da3a02612ae7f31760781f367d1e4fe1abe45c
lichuwen <545759585@qq.com>
2020-07-22 15:24:14 +0800

    [karen] update practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,18 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+    Student(String name, int age,int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        String intro = "I am a Student. I am at Class " + klass + ".";
+        return intro;
+    }
 }
