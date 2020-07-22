ab0e7e2f20a21ba0f26cd16e63f6b0b2e0f00dcd
lichuwen <545759585@qq.com>
2020-07-22 23:03:26 +0800

    [karen] update practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,19 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+    Student(String name, int age,int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        String basicInto = super.introduce() + ' ';
+        String intro = basicInto + "I am a Student. I am at Class " + klass + ".";
+        return intro;
+    }
 }
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,13 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person {
+
+    Worker(String name, int age){
+        super(name,age);
+    }
+    public String introduce(){
+        String basicInto = super.introduce() + ' ';
+        String intro = basicInto + "I am a Worker. I have a job.";
+        return intro;
+    }
 }
